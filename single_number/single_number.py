2dd24ef818394ae57e16970cbdc8126db6c14b8e
CodingCCarpenter <Christineassists@aol.com>
2020-08-13 21:30:01 -0500

    single.py first pass solution

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,7 +5,15 @@ Returns: an integer
 def single_number(arr):
     # Your code here
 
-    pass
+    set_1, set_2 = set(), set()
+
+    for n in arr:
+        if n not in set_1:
+            set_1.add(n)
+        else:
+            set_2.add(n)
+
+    return (set_1 - set_2).pop()
 
 
 if __name__ == '__main__':
